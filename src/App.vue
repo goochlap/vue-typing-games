f8a1bd9a7be98394649cf552e2057d992661d8b1
goochlap <goochlapeyronie@gmail.com>
2022-02-15 18:11:23 +0100

    add bootstrap

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,13 @@ export default {
 </script>
 
 <style>
+html,
+body {
+  min-height: 100vh;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  background: linear-gradient(#7f53ac, #647dee);
 }
 </style>
