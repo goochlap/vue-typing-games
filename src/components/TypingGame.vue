7def31309a169dd1e4ef130f278052da219f058b
gooch lapeyronie <goochlapeyronie@gmail.com>
2022-02-22 15:36:01 +0100

    add difficulty & progress bar

--- src/components/TypingGame.vue
+++ src/components/TypingGame.vue
@@ -5,7 +5,8 @@
   <div class="container text-center">
     <div class="row">
       <div class="col-md-6 mx-auto">
-        <h2 class="display-2 mb-5">
+        <h2 v-if="gameOver" class="display-2 mb-5">Game Over!</h2>
+        <h2 v-else class="display-2 mb-5">
           <span
             v-for="(item, index) in word"
             :key="index"
@@ -14,6 +15,7 @@
             {{ item.letter }}
           </span>
         </h2>
+
         <input
           type="text"
           class="form-control form-control-lg"
@@ -22,7 +24,7 @@
           v-model="input"
         />
         <div class="progress">
-          <div class="progress-bar" role="progressbar"></div>
+          <div class="progress-bar" role="progressbar" :style="increaseBar"></div>
         </div>
         <h4 class="mt-3" id="message"></h4>
         <br />
@@ -42,11 +44,11 @@
             </h3>
           </div>
           <div class="col mt-3 d-flex justify-content-evenly">
-            <label for="input-level"><h3>Difficulty:</h3></label>
-            <select id="input-level">
-              <option>Easy</option>
-              <option>Medium</option>
-              <option>Hard</option>
+            <h3>Difficulty:</h3>
+            <select v-model="selected" :disabled="isActive">
+              <option v-for="option in options" :value="option" :key="option">
+                {{ option.level }}
+              </option>
             </select>
           </div>
           <div class="col mt-3">
@@ -80,11 +82,18 @@ export default {
       isActive: false,
       hidden: false,
       gameOver: false,
-      time: 10,
+      time: 30,
       word: [],
       input: '',
       checker: 0,
-      score: 0
+      score: 0,
+      width: 0,
+      selected: 'Easy',
+      options: [
+        { level: 'Easy', time: 30 },
+        { level: 'Medium', time: 20 },
+        { level: 'Hard', time: 10 }
+      ]
     }
   },
   methods: {
@@ -98,8 +107,9 @@ export default {
         this.time--
         if (this.time === 0) {
           clearInterval(timer)
-          this.time = 10
+          this.time = this.selected.time
           this.gameOver = true
+          this.isActive = false
         }
       }, 1000)
     },
@@ -111,6 +121,9 @@ export default {
       this.word = word.split('').map(letter => {
         return { letter, active }
       })
+    },
+    increaseWidth() {
+      this.width += 10
     }
   },
   watch: {
@@ -134,8 +147,19 @@ export default {
           this.hidden = false
           this.input = ''
           this.checker = 0
+          this.increaseWidth()
           this.initWord()
         }, 500)
+      }
+    },
+    selected(value) {
+      this.time = value.time
+    }
+  },
+  computed: {
+    increaseBar() {
+      return {
+        width: `${this.width}%`
       }
     }
   }
@@ -189,7 +213,7 @@ header {
 }
 
 .progress-bar {
-  width: 0%;
+  /* width: 0%; */
   background-color: #e9f897;
   background-image: linear-gradient(to bottom, #e9f897, #f4e623);
 }
